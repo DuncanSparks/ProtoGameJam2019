3c32bd2fa1632abbddf4adb9a6dbfecb164b9169
Duncan Sparks <duncan.sprks@gmail.com>
2019-10-18 22:28:07 -0700

    Initial commit

---
+++ Scripts/Player.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody
+{
+
+	// ================================================================
+	
+	public override void _Ready()
+	{
+		
+	}
+}
