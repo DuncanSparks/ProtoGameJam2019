854b591b2624472b98e2969b843217b96633cbb7
Duncan Sparks <duncan.sprks@gmail.com>
2019-10-20 11:39:09 -0700

    Add level transition

--- Scripts/PageDisplay.cs
+++ Scripts/PageDisplay.cs
@@ -3,8 +3,26 @@ using System;
 
 public class PageDisplay : CanvasLayer
 {
+	private string nextLevel;
+
 	public void SetText(string text)
 	{
 		GetNode<Label>("Label").Text = text;
+	}
+
+
+	public void SetNextLevel(string level)
+	{
+		nextLevel = level;
+	}
+
+
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionJustPressed("action"))
+		{
+			GetTree().ChangeScene(nextLevel);
+			QueueFree();
+		}
 	}
 }
