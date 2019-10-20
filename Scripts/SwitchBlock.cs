e748ae32122b5178781997ce04e4deb5a2f60adf
Duncan Sparks <duncan.sprks@gmail.com>
2019-10-19 20:16:32 -0700

    Start on switch blocks

---
+++ Scripts/SwitchBlock.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class SwitchBlock : StaticBody
+{
+	[Export]
+	private Vector3 positionAlpha;
+
+	[Export]
+	private Vector3 positionBeta;
+
+	private Vector3 target;
+
+	// ================================================================
+	
+	public override void _Ready()
+	{
+		target = Translation;
+	}
+
+
+	public override void _PhysicsProcess(float delta)
+	{
+		Vector3 pos = Translation;
+		Translation = new Vector3(Controller.LerpDelta(pos.x, target.x, 0.3f, delta), Controller.LerpDelta(pos.y, target.y, 0.3f, delta), pos.z);
+	}
+
+
+	public void Move(bool beta)
+	{
+		target = beta ? positionBeta : positionAlpha;
+	}
+}
